59685b447db6fe58a821deb3cec4517f95038975
saverio <xavier.mitault@epitech.eu>
2022-05-13 18:35:58 +0200

    Move iterration add command pipe to function that check invalid

--- src/parsecommand/parse_pipe.c
+++ src/parsecommand/parse_pipe.c
@@ -5,6 +5,7 @@
 ** parse pipe redirection
 */
 
+#include <stdbool.h>
 #include <unistd.h>
 #include "my_list.h"
 #include "my_strings.h"
@@ -46,7 +47,7 @@ static int fill_pipes(list_t *pipes)
     int len = list_t_len(pipes);
     int status = 0;
 
-    if (len == 1) {
+    if (len <= 1) {
         return (0);
     }
     for (int i = 1; i < len && pipes != NULL && pipes->next != NULL &&
@@ -56,18 +57,25 @@ static int fill_pipes(list_t *pipes)
     return (status);
 }
 
-static list_t *add_to_parsed(list_t *list, char *data, shell_t *shell)
+static list_t *add_to_parsed(list_t *pipes_str, shell_t *shell)
 {
     command_t *cm = NULL;
+    list_t *pipes_cm = NULL;
 
-    cm = parse_single_command(data, shell);
-    if (cm == NULL) {
-        list_t_destroy(list);
-        shell->status_code = 1;
+    if (check_null_command(pipes_str, shell) == false) {
         return (NULL);
     }
-    list = list_t_add(list, cm, free_command_void);
-    return (list);
+    for (int i = 0; i < list_t_len(pipes_str) && shell->status_code == 0;
+            i++, pipes_str = pipes_str->next) {
+        cm = parse_single_command(pipes_str->data, shell);
+        if (cm == NULL) {
+            list_t_destroy(pipes_cm);
+            shell->status_code = 1;
+            return (NULL);
+        }
+        pipes_cm = list_t_add(pipes_cm, cm, free_command_void);
+    }
+    return (pipes_cm);
 }
 
 list_t *parse_pipe(char *input, shell_t *shell)
@@ -81,10 +89,7 @@ list_t *parse_pipe(char *input, shell_t *shell)
         write(2, "Invalid null command.\n", 22);
         return (NULL);
     }
-    for (int i = 0; i < list_t_len(pipes) && shell->status_code == 0;
-            i++, pipes = pipes->next) {
-        piped = add_to_parsed(piped, pipes->data, shell);
-    }
+    piped = add_to_parsed(pipes, shell);
     list_t_destroy(pipes);
     if (fill_pipes(piped) != 0) {
         list_t_destroy(piped);
