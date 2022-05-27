bf90270fde420bbc802536c45ac8b13d8696ab8c
Xavier Mitault <xavier.mitault@epitech.eu>
2022-05-27 20:01:11 +0200

    Add start of default ps1

--- src/utils/correct_env.c
+++ src/utils/correct_env.c
@@ -11,6 +11,8 @@
 #include "my_dico.h"
 #include "my_strings.h"
 
+static const char ps1_custom[] = "";
+
 static const char nls_path_default[] = "/usr/share/locale/%L/LC_MESSAGES/" \
                                         "%N.cat:/usr/share/locale/%l/" \
                                         "LC_MESSAGES/%N.cat";
@@ -75,5 +77,6 @@ dico_t *correct_env(dico_t *env)
     env = add_pwd_user(env);
     env = dico_t_rem(env, "OLDPWD");
     env = add_vars(env);
+    env = dico_t_add_data(env, "PS1", (void *) ps1_custom, NULL);
     return (env);
 }
