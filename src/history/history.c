1cbb5c864ff7dc9aaeca65ebaaee74253737eab7
angeD <ange.duhayon@epitech.eu>
2022-05-27 23:26:12 +0200

    Add get_history_line

--- src/history/history.c
+++ src/history/history.c
@@ -16,14 +16,15 @@ char *get_history_file(dico_t *env)
     const char *file = dico_t_get_value(env, "HISTFILE");
     const char *home = NULL;
     char buf[PATH_MAX] = {0};
-    char filename[] = ".trush_history";
+    char filename[] = ".thrush_history";
 
     if (!file) {
         home = dico_t_get_value(env, "HOME");
         if (!home || strlen(home) > sizeof buf - sizeof filename)
             return NULL;
         strncpy(buf, home, sizeof buf);
-        strcat(buf, "/.trush_history");
+        strcat(buf, "/");
+        strcat(buf, filename);
         dico_t_add_data(env, "HISTFILE", buf, &free);
     }
     return dico_t_get_value(env, "HISTFILE");
@@ -55,23 +56,42 @@ int inc_history_size(dico_t *env, int increment)
     return cmd_number;
 }
 
-int add_to_hist(dico_t *env, char **command)
+char *get_history_line(dico_t *env, int line_nb)
+{
+    const char *histfile = get_history_file(env);
+    FILE *file = fopen(histfile, "r");
+    char *line = NULL;
+
+    if (!file) {
+        return NULL;
+    }
+    while(--line_nb) {
+        if ((fscanf(file, "%*[^\n]"), fscanf(file, "%*c")) == EOF) {
+            fclose(file);
+            return NULL;
+        }
+    }
+    if (getline(&line, (size_t[]){0}, file) < 0) {
+        free(line);
+        line = NULL;
+    }
+    fclose(file);
+    return line;
+}
+
+int add_to_hist(dico_t *env, const char *line)
 {
     const char *histfile = get_history_file(env);
     FILE *file = fopen(histfile, "a+");
-    int i = 0;
 
     if (!file) {
         return -1;
     }
     fseek(file, -1, SEEK_END);
     if (fgetc(file) != '\n') {
-        fprintf(file, "\n");
+        fputs("\n", file);
     }
-    for (; command[i + 1]; i++) {
-        fprintf(file, "%s ", command[i]);
-    }
-    fprintf(file, "%s\n", command[i]);
+    fprintf(file, "%s\n", line);
     inc_history_size(env, 1);
     fclose(file);
     return 0;
