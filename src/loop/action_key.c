b64fb0f97eefa322cc3c982db906c8a067247e02
Xavier Mitault <xavier.mitault@epitech.eu>
2022-05-27 14:24:17 +0200

    Remove the old action ofr key up

--- src/loop/action_key.c
+++ src/loop/action_key.c
@@ -12,24 +12,23 @@
 #include "mysh.h"
 #include "mysh_struct.h"
 
-//TODO: itter through history
-bool use_key_up(shell_t *shell)
+void display_input(shell_t *shell, int *current_pos)
 {
-    if (shell->last_input == NULL) {
-        return (false);
+    for (int i = 0; i < *current_pos - 1; i++) {
+        my_putchar('\b');
+        my_putchar(' ');
+        my_putchar('\b');
     }
-    my_printf("\nHistory arrow not inplemented...\n");
-    if (isatty(0)) {
-        print_prompt(shell);
-        my_putstr(shell->last_input);
+    my_putstr(shell->last_input);
+    for (int i = my_strlen(shell->last_input); i > *current_pos; i--) {
+        my_putchar('\b');
     }
-    return (false);
 }
 
 //TODO: complete binary/path
-bool use_key_tab(shell_t *shell)
+bool use_key_tab(shell_t *shell, __attribute__((unused)) int *cur_pos)
 {
-    if (shell->last_input == NULL) {
+    if (my_strlen(shell->last_input) <= 0) {
         return (false);
     }
     my_printf("\nCompletion not inplemented...\n");
@@ -40,9 +39,9 @@ bool use_key_tab(shell_t *shell)
     return (false);
 }
 
-bool use_key_ctrld(shell_t *shell)
+bool use_key_ctrld(shell_t *shell, __attribute__((unused)) int *cur_pos)
 {
-    if (shell->last_input == NULL) {
+    if (my_strlen(shell->last_input) <= 0) {
         shell->is_end = true;
         return (true);
     }
@@ -54,14 +53,23 @@ bool use_key_ctrld(shell_t *shell)
     return (false);
 }
 
-bool use_key_backspace(shell_t *shell)
+bool use_key_backspace(shell_t *shell, int *cur_pos)
 {
-    if (my_strlen(shell->last_input) <= 0) {
+    int tmp_len = my_strlen(shell->last_input);
+
+    if (tmp_len <= 0 || *cur_pos <= 0) {
         return (false);
     }
-    my_putchar('\b');
-    my_putchar(' ');
-    my_putchar('\b');
-    shell->last_input[my_strlen(shell->last_input) - 1] = '\0';
+    for (int i = 0; i < tmp_len; i++) {
+        my_putchar('\b');
+        my_putchar(' ');
+        my_putchar('\b');
+    }
+    *cur_pos -= 1;
+    for (int i = 0; i < tmp_len - 2; i++) {
+        my_putchar(' ');
+    }
+    my_strreminsert(shell->last_input, *cur_pos);
+    display_input(shell, cur_pos);
     return (false);
 }
