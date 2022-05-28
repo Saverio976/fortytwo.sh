81d3b0ec42eaf6f92ab19ed19fecf5bb89359194
Xavier Mitault <xavier.mitault@epitech.eu>
2022-05-28 12:49:06 +0200

    Add history with !

--- src/parsecommand/parse_command.c
+++ src/parsecommand/parse_command.c
@@ -7,12 +7,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "loop.h"
 #include "my_strings.h"
 #include "my_clear_str.h"
 #include "my_list.h"
 #include "mysh_struct.h"
 #include "my_wordarray.h"
 #include "mysh.h"
+#include "my_puts.h"
 #include "history.h"
 
 list_t *or_separator(list_t *list)
@@ -63,24 +65,6 @@ list_t *ampersand_separator(list_t *list)
     return (new);
 }
 
-char *my_wordarray_to_str(char **wordarray)
-{
-    int i = 0;
-    int len = my_wordarray_size(wordarray);
-    int max = my_wordarray_len(wordarray);
-    char *str = my_calloc(len);
-
-    if (wordarray == NULL || str == NULL)
-        return (NULL);
-    while (i < max) {
-        str = my_strcat(str, wordarray[i]);
-        if (i < max - 1)
-            str = my_strcat(str, " ");
-        i++;
-    }
-    return (str);
-}
-
 list_t *check_alias(list_t *cm, list_t *alias)
 {
     int change = 0;
@@ -98,24 +82,47 @@ list_t *check_alias(list_t *cm, list_t *alias)
     return cm;
 }
 
+char *get_string_with_history(char *string, shell_t *shell)
+{
+    char *tmp[2] = {0};
+    int nb_hist = 0;
+    int max_hist = 0;
+
+    if (string == NULL || my_strstartswith(string, "!") != 1) {
+        return (string);
+    }
+    max_hist = inc_history_size(shell->env, 0);
+    nb_hist = (my_strlen(string) == 1) ? max_hist :
+        find_history_backward(shell->env, string + 1, max_hist);
+    tmp[0] = get_history_line(shell->env, nb_hist);
+    if (tmp[0] == NULL) {
+        my_puterror(string + 1, "Event not found.\n");
+    }
+    tmp[1] = my_strstrip(tmp[0], "\n ");
+    free_secure(string);
+    free_secure(tmp[0]);
+    my_printf("%s\n", (tmp[1] == NULL) ? "" : tmp[1]);
+    return (tmp[1]);
+}
+
 int parse_commands(char *string, shell_t *shell)
 {
     char **arr = NULL;
 
-    arr = my_wordarray_from_str(string, ' ');
+    shell->last_input = get_string_with_history(string, shell);
+    arr = my_wordarray_from_str(shell->last_input, ' ');
     if (arr == NULL) {
         shell->status_code = 0;
         return (1);
     }
     my_wordarray_free(arr);
     list_t_destroy(shell->command);
-    add_to_hist(shell->env, string);
-    shell->last_input = replace_value_env(shell->env, string);
+    add_to_hist(shell->env, shell->last_input);
+    shell->last_input = replace_value_env(shell->env, shell->last_input);
     shell->command = my_strsplit(shell->last_input, ";");
     shell->command = ampersand_separator(shell->command);
     shell->command = or_separator(shell->command);
     shell->command = remove_empty_commands(shell->command);
     shell->command = check_alias(shell->command, shell->alias);
-    free(string);
     return (0);
 }
