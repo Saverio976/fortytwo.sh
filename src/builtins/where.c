8102816d486a5a09093722147beb004b5bee7acf
tenshi <tenshii.tv@gmail.com>
2022-05-28 16:07:26 +0200

    fix where return value

--- src/builtins/where.c
+++ src/builtins/where.c
@@ -15,12 +15,6 @@
 #include "my_puts.h"
 #include "my_wordarray.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <string.h>
 #include <string.h>
 
 static char **my_split_command(char *command, char *limit)
@@ -45,45 +39,58 @@ static char **my_split_command(char *command, char *limit)
     return (tab);
 }
 
-static void print_path(char *cmd, char *path)
+static int print_path(char *cmd, char *path)
 {
+    int size = 0;
+    int return_value = 1;
     char **path_array = NULL;
 
     path_array = my_split_command(path, ":");
     for (int i = 0; path_array[i] != NULL; i += 1) {
-        path = malloc(sizeof(char) * (my_strlen(path_array[i]) + my_strlen(cmd) + 2));
+        size = (my_strlen(path_array[i]) + my_strlen(cmd) + 2);
+        path = malloc(sizeof(char) * size);
         if (path == NULL)
             continue;
         my_strcpy(path, path_array[i]);
         my_strcat(path, "/");
         my_strcat(path, cmd);
-        if (access(path, F_OK) == 0)
+        if (access(path, F_OK) == 0) {
             printf("%s\n", path);
+            return_value = 0;
+        }
         free(path);
     }
     my_wordarray_free(path_array);
+    return (return_value);
 }
 
-static void execute_where(shell_t *shell, char *command)
+static int execute_where(shell_t *shell, char *command)
 {
     char *path = NULL;
+    int return_value = 1;
+    int builtin_return_value = 1;
 
     if (check_if_builtins(command) == true) {
         my_printf("%s is a shell built-in\n", command);
+        builtin_return_value = 0;
     }
     path = my_strdup(getenv("PATH"));
     if (path == NULL)
         path = my_strdup("/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin");
-    print_path(command, path);
+    return_value = print_path(command, path);
     if (path != NULL)
         free(path);
+    if (return_value == 1 && builtin_return_value == 1)
+        return (1);
+    return (0);
 }
 
 void where_builtins(shell_t *shell, command_t *command)
 {
+    int return_value = 0;
     int nb_args = my_wordarray_len(command->arguments);
-    shell->status_code = 0;
 
+    shell->status_code = 0;
     if (nb_args < 2) {
         my_puterror("where", "Too few arguments.\n");
         shell->status_code = 1;
@@ -94,9 +101,11 @@ void where_builtins(shell_t *shell, command_t *command)
         strncmp(command->arguments[i], "/", 1) == 0 ||
         strncmp(command->arguments[i], "../", 3) == 0) {
             puts("where: / in command makes no sense");
-            shell->status_code = 1;
+            return_value = 1;
             continue;
         }
-        execute_where(shell, command->arguments[i]);
+        return_value += execute_where(shell, command->arguments[i]);
     }
+    if (return_value >= 1)
+        shell->status_code = 1;
 }
