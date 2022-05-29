20d17042dfba32438dee26b5175f1d104150eeaa
swann lagoute <zoreil34@localhost.localdomain>
2022-05-29 18:19:34 +0200

    Add final

--- src/utils/correct_env.c
+++ src/utils/correct_env.c
@@ -11,7 +11,9 @@
 #include "my_dico.h"
 #include "my_strings.h"
 
-static const char ps1_custom[] = "";
+static const char ps1_custom[] = "\033[1;33m╭─\\A \033[1;36m\\u \033[0;34m->" \
+                                    " \033[0;34m:(\033[1;31m\\W\033[0;34m) " \
+                                    "\033[1;33m✗\n╰─ ~ \033[1;30m ";
 
 static const char nls_path_default[] = "/usr/share/locale/%L/LC_MESSAGES/" \
                                         "%N.cat:/usr/share/locale/%l/" \
